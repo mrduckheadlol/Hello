bde3a8a7fe1d43b5a1654ea92360834887426670
mrduckheadlol <153369273+mrduckheadlol@users.noreply.github.com>
2023-12-09 20:32:34 +0700

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	 
+	int x;
+	x = 10;
+	x = 20;
+	
+	float pi = 3.14159;
+	
+	printf("Gia Tri = %d\n",x);
+ 
+	x = 30;
+	 printf("Gia Tri = %d\n",x);
+	 x = 45; 
+	 printf("Gia Tri = %d\n",x);
+	 x = x + 135;
+	 printf("Gia Tri = %f\n",pi);
+}
+
